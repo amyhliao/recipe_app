c13d9580096d6c5f29feb79f6ec6a86781706308
Amy Liao <amyhliao88@gmail.com>
2016-02-10 23:11:54 -0800

    add methods to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,21 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
 
   validates :title, :directions, :prep_time, :cook_time, :total_time, presence: true
+
+  def count_votes
+    total = self.votes.sum(:value)
+    self.update_attribute(:vote_count, total)
+  end
+
+  # def sorted_reviews
+  #   if best_review_id
+  #     best_rev = Review.find(best_review_id)
+  #     rev_arr = self.reviews.select{|rev| rev.id != best_review_id}
+  #     sort_array_by_points(rev_arr).unshift(best_rev)
+  #   else
+  #     rev_arr = self.reviews.dup
+  #     sort_array_by_points(rev_arr)
+  #   end
+  # end
+
 end
