6a540fb758b38205079822c5c8264e8fbfe7bdba
Amy Liao <amyhliao88@gmail.com>
2016-02-12 22:33:48 -0800

    edit category model method

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
 
-   def self.make_categories(categories, recipe)
+  def self.make_categories(categories, recipe)
     category_array = categories.split(", ")
     category_array.each do |new_category|
       old_category = Category.find_by(name: new_category)
