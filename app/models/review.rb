d3e5122634a13e5d73c7995d316c8de48fef8a82
Amy Liao <amyhliao88@gmail.com>
2016-02-13 22:58:16 -0800

    add voting method to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,4 +13,8 @@ class Review < ActiveRecord::Base
     total = self.votes.sum(:value)
     self.update_attribute(:vote_count, total)
   end
+
+  def vote_on_this?(current_user_id)
+    !Vote.find_by(voter_id: current_user_id, votable_id: self.id, votable_type: "Response") && current_user_id != self.responder.id
+  end
 end
