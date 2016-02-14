8f23c306e13b200ccb55eb55917bd5772e8e511f
Amy Liao <amyhliao88@gmail.com>
2016-02-13 23:17:03 -0800

    add new method to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def new
+    if current_user != nil
+      @parent = parent_object
+      @parent_name = @parent.class.name.downcase.pluralize
+      @response = Review.new
+      if request.xhr?
+        render 'reviews/_review_form.html.erb', locals: {parent: @parent, review: @review}, layout: false
+      else
+        render 'reviews/new.html.erb'
+      end
+    else
+      redirect_to login_path
+    end
+  end
+
 end
