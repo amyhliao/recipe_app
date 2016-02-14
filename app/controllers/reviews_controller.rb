213b09b3f756e88b5821d9d4c2a2065e11296d50
Amy Liao <amyhliao88@gmail.com>
2016-02-13 23:18:16 -0800

    add create method to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
     if current_user != nil
       @parent = parent_object
       @parent_name = @parent.class.name.downcase.pluralize
-      @response = Review.new
+      @review = Review.new
       if request.xhr?
         render 'reviews/_review_form.html.erb', locals: {parent: @parent, review: @review}, layout: false
       else
@@ -11,6 +11,21 @@ class ReviewsController < ApplicationController
       end
     else
       redirect_to login_path
+    end
+  end
+
+  def create
+    @parent = parent_object
+    @review = @parent.reviews.build(responder_id: current_user.id, content: params[:review][:content])
+    if @review.save
+      if request.xhr?
+        render 'reviews/_review.html.erb', locals: {review: @review}, layout: false
+      else
+        redirect_to parent_url(@parent)
+      end
+    else
+      flash.now[:alert] = "Review must have content."
+      render :new
     end
   end
 
