41d49367733f9e751547894e9148a81c4be42d60
Amy Liao <amyhliao88@gmail.com>
2016-02-14 20:35:34 -0800

    add new method to ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,12 @@
 class IngredientsController < ApplicationController
+  def new
+    if logged_in?
+      @recipe = Recipe.new
+      @category = Category.new
+    else
+      redirect_to login_path
+    end
+  end
+
+
 end
